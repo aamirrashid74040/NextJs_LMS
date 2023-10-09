dfea953c60964b400dd61102d1f523aeda9b79e2
Aamir Rashid <aamirrashid74040@gmail.com>
2023-10-09 10:50:38 +0500

    Changing the design of icon badge component from full rounded to rouded md

--- components/icon-badge.jsx
+++ components/icon-badge.jsx
@@ -3,25 +3,22 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 // variants for background color of the icon badge
-const backgroundVariants = cva(
-  "rounded-full flex items-center justify-center",
-  {
-    variants: {
-      variant: {
-        default: "bg-sky-100",
-        success: "bg-emerald-100",
-      },
-      size: {
-        default: "p-2",
-        sm: "p-1",
-      },
+const backgroundVariants = cva("rounded-md flex items-center justify-center", {
+  variants: {
+    variant: {
+      default: "bg-sky-100",
+      success: "bg-emerald-100",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "p-2",
+      sm: "p-1",
     },
-  }
-);
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 const iconVariants = cva("", {
   variants: {
